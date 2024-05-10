ff1e4c096c6c07b5532c0b1d6ab9645a3e82edaf
feelingalonehere <168257131+feelingalonehere@users.noreply.github.com>
2024-05-10 06:49:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-function currentTime() {
-    let date = new Date(); 
-    let mmm = date.toLocaleString('default', { month: 'short' });
-    let yyy = date.getFullYear();
-    let ddd = date.getDate();
-    let hh = date.getHours();
-    let mm = date.getMinutes();
-    let ss = date.getUTCSeconds();
-    let session = "AM";
-  
-    if(hh == 0){
-        hh = 12;
-    }
-    if(hh > 12){
-        hh = hh - 12;
-        session = "PM";
-     }
-  
-    hh = (hh < 10) ? "0" + hh : hh;
-    mm = (mm < 10) ? "0" + mm : mm;
-    ss = (ss < 10) ? "0" + ss : ss;
-    
-    let time = hh + ":" + mm + ":" + ss + " " + session;
-    let mdy = mmm + ", " + ddd + ", " + yyy 
-    document.getElementById("mmddyy").innerText = mdy;
-    document.getElementById("clock").innerText = time; 
-    
-    let t = setTimeout(function(){ currentTime() }, 1000);
+setInterval(setClock, 1000);
+
+const hourHands = document.querySelector("[data-hour-hand]");
+const minHands = document.querySelector("[data-min-hand]");
+const secHands = document.querySelector("[data-sec-hand]");
+
+function setClock() {
+  const currentDate = new Date();
+  const secRatio = currentDate.getSeconds() / 60;
+  const minRatio = currentDate.getMinutes() / 60;
+  const hourRatio = (currentDate.getHours() + minRatio) / 12;
+  setRotation(secHands, secRatio);
+  setRotation(minHands, minRatio);
+  setRotation(hourHands, hourRatio);
 }
-// https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_qDpaX67IqAO5dubsJAjVm65TVccCt&ipAddress=8.8.8.8
-currentTime()
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setClock();
